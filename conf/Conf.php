7fbcfaa381ed6aeec2d5e68625971ad1c276d387
amaury-jaspar <amaury.jaspar@etu.umontpellier.fr>
2019-10-16 10:18:24 +0200

    changed database hostname

--- conf/Conf.php
+++ conf/Conf.php
@@ -2,38 +2,38 @@
 
 class Conf {
 
-    static private $databases = array (
-        'hostname' => 'https://webinfo.iutmontp.univ-montp2.fr',
-        // le nom de la base de donnée
-        'database' => 'dominionVente',
-        // root
-        'login' => 'simondonj',
-        // mdp créer à l'installation, certainement un root
-        'password' => '07C4XS02ID7'
-        // a remplir
-    );
+	static private $databases = array (
+		'hostname' => 'https://webinfo.iutmontp.univ-montp2.fr',
+		// le nom de la base de donnée
+		'database' => 'simondonj',
+		// root
+		'login' => 'simondonj',
+		// mdp créer à l'installation, certainement un root
+		'password' => '07C4XS02ID7'
+		// a remplir
+	);
 
-    static private $debug = True;
+	static private $debug = True;
 
-    static public function getDebug() {
-        return self::$debug;
-    }
+	static public function getDebug() {
+		return self::$debug;
+	}
 
-    static public function getLogin() {
-        return self::$databases['login'];
-      }
+	static public function getLogin() {
+		return self::$databases['login'];
+	  }
 
-    static public function getDatabase() {
-        return self::$databases['database'];
-    }
+	static public function getDatabase() {
+		return self::$databases['database'];
+	}
 
-    static public function getHostname() {
-        return self::$databases['hostname'];
-    }
+	static public function getHostname() {
+		return self::$databases['hostname'];
+	}
 
-    static public function getPassword() {
-        return self::$databases['password'];
-    }
+	static public function getPassword() {
+		return self::$databases['password'];
+	}
 
 }
 
