ad99ea080db78159a9e76b918f8fb21698d65a37
amaury-jaspar <amaury.jaspar@etu.umontpellier.fr>
2019-10-16 12:02:00 +0200

    added ControllerUser

--- controller/ControllerUser.php
+++ controller/ControllerUser.php
@@ -1,39 +1,39 @@
 <?php
 
-    require_once (File::build_path(array('model', 'ModelUser.php')));
+	require_once (File::build_path(array('model', 'ModelUser.php')));
 
-    class ControllerUser {
+	class ControllerUser {
 
-        protected static $object = "user";
+		protected static $object = "user";
 
-        public static function read() {
+		public static function read() {
+			echo "Welcome to the Pied de Chaise";
+		}
 
-        }
+		public static function readAll() {
 
-        public static function readAll() {
+		}        
 
-        }        
+		public static function create() {
 
-        public static function create() {
+		}
 
-        }
+		public static function created() {
+		
+		}
 
-        public static function created() {
-        
-        }
+		public static function delete() {
 
-        public static function delete() {
+		}
 
-        }
+		public static function update() {
+		
+		}
 
-        public static function update() {
-        
-        }
+		public static function updated() {
 
-        public static function updated() {
+		}
 
-        }
-
-    }
+	}
 
 ?>
