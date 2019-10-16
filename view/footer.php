7dc171e599c0a400de4c3f8e736e39d683f99229
mathieu-LAGNY <mathieu.lagny@etu.umontpellier.fr>
2019-10-16 11:24:18 +0200

    maj header and footer

--- view/footer.php
+++ view/footer.php
@@ -5,12 +5,12 @@ echo <<< EOT
           <div class="container">
             <div class="row">
               <div class="col l6 s12">
-                <h5 class="white-text">DOMINION</h5>
-                <p class="grey-text text-lighten-4">Site de vente de carte d'Item pour aventurier</p>
+                <h5 class="white-text">Mystic Market Everywhere</h5>
+                <p class="grey-text text-lighten-4">Site selling equipment for adventurers</p>
               </div>
               <div class="col l4 offset-l2 s12">
                 <ul>
-                  <li><a class="grey-text text-lighten-3" href="index.php?action=contact&controller=accueil">Contact</a></li>
+                  <li><a class="grey-text text-lighten-3" href="index.php?action=contact&controller=home">Contact</a></li>
 EOT;
 if ($_SESSION['admin'] == true) {
                     echo '<li><a class="grey-text text-lighten-3" href="index.php?action=dashboard&controller=administration">Administration</a></li>';
--- view/header.php
+++ view/header.php
@@ -4,26 +4,26 @@ echo <<< EOT
 <header>
     <nav>
     <div class="nav-wrapper">
-        <a href="index.php?action=pageDeGardeBuilder&controller=accueil" class="brand-logo">Le marché de l'aventurier</a>
+        <a href="index.php?action=pageDeGardeBuilder&controller=home" class="brand-logo">Mystic Market Everywhere</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
 EOT;
         if(!empty($_SESSION['login'])) { echo '<li class="green">vous êtes connecté</li>'; }
 echo <<< EOT
-        <li><a href="index.php?action=pageDeGardeBuilder&controller=accueil">Accueil</a></li>
-                <li><a href="index.php?action=pagination&controller=produit">Catalogue</a></li>
-                <li><a href="index.php?action=readBasket&controller=produit">Panier</a></li>
+        <li><a href="index.php?action=pageDeGardeBuilder&controller=home">Home</a></li>
+                <li><a href="index.php?action=pagination&controller=product">Catalog</a></li>
+                <li><a href="index.php?action=readBasket&controller=product">Basket</a></li>
 EOT;
 
 if (!empty($_SESSION['login'])) {
-                echo '<li><a href="index.php?action=read&controller=inventory">Inventaire</a></li>';
-                echo '<li><a href="index.php?action=read&controller=wishList">Liste de souhait</a></li>';
-                echo '<li><a href="index.php?action=pageBuilder&controller=utilisateur'."&login=".$_SESSION['login'].'">Profil</a></li>';
-                echo '<li><a class="waves-effect waves-light btn-small" href="index.php?action=disconnect&controller=utilisateur">Se Déconnecter</a></li>';
+                echo '<li><a href="index.php?action=read&controller=inventory">Inventory</a></li>';
+                echo '<li><a href="index.php?action=read&controller=wishList">Wish List</a></li>';
+                echo '<li><a href="index.php?action=pageBuilder&controller=user'."&login=".$_SESSION['login'].'">Profile</a></li>';
+                echo '<li><a class="waves-effect waves-light btn-small" href="index.php?action=disconnect&controller=user">Log out</a></li>';
 }
 
 if (empty($_SESSION['login'])) {
-                echo '<li><a href="index.php?action=create&controller=utilisateur">S\'inscrire</a></li>';
-                echo '<li><a class="waves-effect waves-light btn-small" href="index.php?action=connect&controller=utilisateur">Se connecter</a></li>';
+                echo '<li><a href="index.php?action=create&controller=user">Register</a></li>';
+                echo '<li><a class="waves-effect waves-light btn-small" href="index.php?action=connect&controller=user">Login</a></li>';
     }
 echo <<< EOT
             </ul>
