a764599437b6581d8236d26c8ad4a8a1f4abe2fb
amaury-jaspar <amaury.jaspar@etu.umontpellier.fr>
2019-10-30 11:57:59 +0100

    re-added controller

---
+++ controller/ControllerItem.php
@@ -0,0 +1,38 @@
+<?php
+
+class ControllerItem {
+
+	protected static $object = "item";
+
+	public static function read() {
+		echo "Ceci est un item";
+	}
+
+	public static function readAll() {
+
+	}        
+
+	public static function create() {
+
+	}
+
+	public static function created() {
+	
+	}
+
+	public static function delete() {
+
+	}
+
+	public static function update() {
+	
+	}
+
+	public static function updated() {
+
+	}
+
+
+}
+
+?>
