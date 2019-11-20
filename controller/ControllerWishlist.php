c94446e5c4651a42d540e3b2115e239c6bbb0c1f
amaury-jaspar <amaury.jaspar@etu.umontpellier.fr>
2019-11-20 11:55:41 +0100

    use selectWhere from Model rather than selectAll in ModelWishlist

--- controller/ControllerWishlist.php
+++ controller/ControllerWishlist.php
@@ -11,15 +11,15 @@ class ControllerWishlist {
   protected static $object = 'wishlist';
 
   public static function read() {
-    $user_id = $_SESSION['login'];
-    $tab_item = ModelWishlist::selectAll($user_id);
+    $login_user = $_SESSION['login'];
+    $tab_item = ModelWishlist::selectWhere($login_user, 'login_user');
     $view = 'list';
     $pagetitle = 'Wishlist';
     require(File::build_path(array('view','view.php')));
   }
 
   public static function addItem() {
-    $user_id = $_SESSION['login'];
+    $login_user = $_SESSION['login'];
     $item_id = Routeur::myGet('item_id');
   }
 
